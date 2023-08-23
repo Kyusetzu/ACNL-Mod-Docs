865f5ec5f14ae7b1ea7c9887f398a89e13c9b21e
Kyusetzu <kyusetzu@gmail.com>
2023-08-23 17:23:04 +0200

    Graph test

--- makegraph.py
+++ makegraph.py
@@ -2,65 +2,62 @@ import os
 import re
 import networkx as nx
 import matplotlib
-matplotlib.use('Agg')  # Setzen des Backends auf 'Agg'
+matplotlib.use('Agg')  # Setting the backend to 'Agg' for compatibility with environments lacking a display
 import matplotlib.pyplot as plt
 from matplotlib.patheffects import withStroke
 
-# Pfad zum Ordner 'docs'
+# Directory path for the 'docs' folder
 path = 'docs'
 
-# Farbvariablen
-NODE_COLOR = "#FFAA00"
-BACKGROUND_COLOR = "#323232"  # Wird nur verwendet, wenn IS_TRANSPARENT False ist
-EDGE_COLOR = "white"
-FONT_COLOR = "black"
-FONT_OUTLINE_COLOR = "white"
+# Color variables for various elements
+NODE_COLOR = "#97c2fc"
+NODE_BORDER_COLOR = "#668fc8"
+BACKGROUND_COLOR = "#2e303d"  # Only used if IS_TRANSPARENT is False
+EDGE_COLOR = "#3d4b65"
+FONT_COLOR = "white"
+FONT_OUTLINE_COLOR = "#668fc8"
 
-# Toggle für transparenten Hintergrund
-IS_TRANSPARENT = True  # Setzen Sie dies auf False, um den Hintergrundfarbenwert zu verwenden
+# Toggles 
+IS_TRANSPARENT = True  # Set to False if you want to use the BACKGROUND_COLOR
+SHOW_FILENAMES = False  # Set to False to hide filenames
+SHOW_NODE_BORDER = True  # Set to False to hide node borders
 
-# Pfad zum Ordner 'docs'
-path = 'docs'
 
-# Alle .md Dateien sammeln
-files = []
-for root, dirs, filenames in os.walk(path):
-    for filename in filenames:
-        if filename.endswith('.md'):
-            files.append(os.path.join(root, filename))
+NODE_DISTANCE = 1
+FONT_SIZE = 8
 
-# Dateititel aus Dateinamen extrahieren (ohne Erweiterung)
+
+
+# Collect all .md files from the 'docs' directory
+files = [os.path.join(root, filename) for root, _, filenames in os.walk(path) for filename in filenames if filename.endswith('.md')]
+
+# Extract titles from filenames (excluding the extension)
 file_titles = {f: os.path.splitext(os.path.basename(f))[0] for f in files}
 
-# Graph erstellen
+# Initialize a graph
 G = nx.Graph()
 
-# Jede Datei zum Graphen als Knoten hinzufügen
+# Add each file as a node to the graph
 for title in file_titles.values():
     G.add_node(title)
 
-# Jede Datei im Verzeichnis durchsuchen
+# Scan each file in the directory for links in the format [[title]]
 for file, title in file_titles.items():
     with open(file, 'r', encoding='utf-8') as f:
         content = f.read()
-        # Für jede Verknüpfung im Format [[titel]] in der Datei
         for match in re.findall(r'\[\[(.*?)\]\]', content):
-            # Wenn der gefundene Titel in der Liste der Dateititel ist
             if match in file_titles.values():
-                # Füge Kanten zum Graphen hinzu
                 G.add_edge(title, match)
 
-# Die Knotengröße festlegen
-node_size_default = 350  # Dies ist die Standardgröße für alle Knoten
+# Default node size
+node_size_default = 10
 
-# Die Knotengröße basierend auf deren Grad (Anzahl der Verbindungen) festlegen
-node_size = [node_size_default + (v * 100) for v in dict(G.degree()).values()]
+# Determine the size of each node based on its degree (number of connections)
+node_size = [node_size_default + (v * 10) for v in dict(G.degree()).values()]
 
+# Generate positions for each node using the spring layout
+pos = nx.spring_layout(G, NODE_DISTANCE)  # 'k' is a parameter that controls distance between nodes
 
-# Positionen für alle Knoten erzeugen
-pos = nx.spring_layout(G, k=1)  # k ist ein Parameter, den Sie anpassen können, um die Knoten näher oder weiter voneinander entfernt anzuordnen
-
-# Graph zeichnen
 plt.figure(figsize=(12, 12))
 
 if IS_TRANSPARENT:
@@ -73,20 +70,20 @@ nx.draw(G, pos,
         node_size=node_size,
         node_color=NODE_COLOR,
         edge_color=EDGE_COLOR,
-        edgecolors="black",
-        linewidths=1,
+        edgecolors=NODE_BORDER_COLOR if SHOW_NODE_BORDER else NODE_COLOR,
+        linewidths=1 if SHOW_NODE_BORDER else 0,
         alpha=0.9
        )
 
-# Fügen Sie die Titel separat mit einer Umrandung hinzu
-for node, (x, y) in pos.items():
-    plt.text(x, y, node, color=FONT_COLOR, fontsize=10, ha='center', va='center',
-             path_effects=[withStroke(linewidth=3, foreground=FONT_OUTLINE_COLOR)])
+if SHOW_FILENAMES:
+    for node, (x, y) in pos.items():
+        plt.text(x, y, node, color=FONT_COLOR, fontsize=FONT_SIZE, ha='center', va='center',
+                 path_effects=[withStroke(linewidth=3, foreground=FONT_OUTLINE_COLOR)])
 
-# Überprüfen Sie, ob der 'assets' Ordner in 'docs' existiert, andernfalls erstellen Sie ihn
+# Check if the 'assets' folder exists in 'docs', if not create it
 assets_path = os.path.join(path, 'assets')
 if not os.path.exists(assets_path):
     os.makedirs(assets_path)
 
-# Save the file
-plt.savefig(os.path.join(path, 'assets', 'graph.png'), facecolor=("none" if IS_TRANSPARENT else BACKGROUND_COLOR), transparent=IS_TRANSPARENT)
+# Save the graph as a PNG file in the 'assets' directory
+plt.savefig(os.path.join(assets_path, 'graph.png'), facecolor=("none" if IS_TRANSPARENT else BACKGROUND_COLOR), transparent=IS_TRANSPARENT)
